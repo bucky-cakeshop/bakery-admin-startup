ed4caa973606442e3d8bd3a8554d2ebef170f7e9
Ariel Grillo <arielgrillo@gmail.com>
2023-05-18 23:21:11 +0200

    Fixed startup files

--- Startup_latest.ps1
+++ Startup_latest.ps1
@@ -2,10 +2,12 @@ $CurrentDirectory = Get-Location
 
 # Absolute path to backend repository
 cd c:\Code\bucky-cakeshop\bakery-admin-backend\
+git pull
 python manage.py runserver &
 
 # Absolute path to frontend repository
 cd c:\Code\bucky-cakeshop\bakery-admin-frontend\
+git pull
 npm start &
 
 Write-Host "Wite while server is loading"
