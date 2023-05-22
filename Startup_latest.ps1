aeda22b3ec38ba032299e2c6d507072099dd1b07
Ariel Grillo <arielgrillo@gmail.com>
2023-05-22 20:05:54 +0200

    migrate and npm install

--- Startup_latest.ps1
+++ Startup_latest.ps1
@@ -3,11 +3,13 @@ $CurrentDirectory = Get-Location
 # Absolute path to backend repository
 cd c:\Code\bucky-cakeshop\bakery-admin-backend\
 git pull
+python manage.py migrate
 python manage.py runserver &
 
 # Absolute path to frontend repository
 cd c:\Code\bucky-cakeshop\bakery-admin-frontend\
 git pull
+npm install
 npm start &
 
 Write-Host "Wite while server is loading"
