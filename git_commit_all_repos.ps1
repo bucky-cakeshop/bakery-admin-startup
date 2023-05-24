f1d474307d6e55a2024bb060f444dcfc2e1365ac
Ariel Grillo <arielgrillo@gmail.com>
2023-05-24 23:36:10 +0200

    Última versión, FE BE Commit.

--- git_commit_all_repos.ps1
+++ git_commit_all_repos.ps1
@@ -1,14 +1,25 @@
 $currentDirectory = Get-Location
 $rootPath=$args[0]
-$comment=$args[1]
+# $comment=$args[1]
 
-write-host $rootPath
-write-host $comment
+$Default_Message = "Última versión, FE BE Commit."
+$BE_Message = Read-Host -Prompt "BE commit message"
+$FE_Message = Read-Host -Prompt "FE commit message"
+
 
 $files = Get-ChildItem $rootPath
 $gitFolder = ".\.git"
 for ($i=0; $i -lt $files.Count; $i++) {
     $outfile = $files[$i].FullName
+	$comment = $Default_Message
+	if($outfile -like "*frontend*"){
+		Write-Host $outfile " IS FE"
+		$comment = $FE_Message
+	}elseif ($outfile -like "*backend*") {
+		Write-Host $outfile " IS BE"
+		$comment = $BE_Message
+	}
+
 	Set-Location $outfile 
 	if (Test-Path -Path $gitFolder) {
 		write-host "Git Repository" -BackgroundColor DarkGreen
